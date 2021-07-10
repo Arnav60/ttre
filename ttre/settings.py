13aa2a737eb6d5d291be72c6cb44a05288b9f55d
Arnav Awasthi <arnavawasthy60@gmail.com>
2021-07-10 22:21:47 +0530

    added inquiry check and sending email using smtp

--- ttre/settings.py
+++ ttre/settings.py
@@ -147,3 +147,10 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
+#Send Email
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'arnavawasthy61@gmail.com'
+EMAIL_HOST_PASSWORD = 'ghajini2.0'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
