04cf56c3ca93fbf5056a3c3906a1cf45e59c512e
Arnav Awasthi <arnavawasthy60@gmail.com>
2021-07-11 20:38:16 +0530

    added another env variable

--- ttre/settings.py
+++ ttre/settings.py
@@ -16,12 +16,12 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
 import os
 from dotenv import load_dotenv
-
+load_dotenv()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sd!_g^t1a(lrd#099^3d1f-x=%tkkh=276(e10v+qva_k%(u89'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -146,13 +146,12 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
-load_dotenv()
 #Send Email
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'arnavawasthy61@gmail.com'
-EMAIL_HOST_PASSWORD = os.getenv("DJANGO_SECRET_KEY")
+EMAIL_HOST_PASSWORD = os.getenv("MAIL_SECRET_KEY")
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 #local settings
