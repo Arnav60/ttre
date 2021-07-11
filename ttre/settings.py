d0ede4f0ce9a0c6d4d9decd446857f6271de0e5e
Arnav Awasthi <arnavawasthy60@gmail.com>
2021-07-11 19:43:46 +0530

    added environment variables to hide secret key in settings file for the git repository

--- ttre/settings.py
+++ ttre/settings.py
@@ -15,7 +15,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 import os
-
+from dotenv import load_dotenv
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -146,11 +146,11 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
-
+load_dotenv()
 #Send Email
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'arnavawasthy61@gmail.com'
-EMAIL_HOST_PASSWORD = 'ghajini2.0'
+EMAIL_HOST_PASSWORD = os.getenv("DJANGO_SECRET_KEY")
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
