682a6524dc84e749631d09b692269ca419b5d723
Arnav Awasthi <arnavawasthy60@gmail.com>
2021-07-18 18:31:58 +0530

    commit

--- ttre/settings.py
+++ ttre/settings.py
@@ -28,8 +28,7 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['ttrealestate.herokuapp.com', 'localhost']
 
 # Application definition
 
