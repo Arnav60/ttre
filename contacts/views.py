f244b12623e0bd64dff40c33c574bc6d2cbce6dc
Arnav Awasthi <arnavawasthy60@gmail.com>
2021-07-19 17:31:31 +0530

    edit

--- contacts/views.py
+++ contacts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import redirect, render
 from . models import Contact
 from django.contrib import messages
 from django.core.mail import send_mail
+
+
 def contact(request):
     if request.method == 'POST':
         listing_id = request.POST['listing_id']
@@ -12,24 +14,28 @@ def contact(request):
         message = request.POST['message']
         user_id = request.POST['user_id']
         realtor_email = request.POST['realtor_email']
-        #Check if user has made an inquiry already
+        # Check if user has made an inquiry already
         if request.user.is_authenticated:
             user_id = request.user.id
-            has_contacted = Contact.objects.all().filter(listing_id=listing_id , user_id=user_id)
+            has_contacted = Contact.objects.all().filter(
+                listing_id=listing_id, user_id=user_id)
             if has_contacted:
-                messages.error(request , 'You have already inquired about this listing!')
+                messages.error(
+                    request, 'You have already inquired about this listing!')
                 return redirect('/listings/'+listing_id)
-        contact = Contact(listing=listing , listing_id=listing_id , name=name , email=email , phone=phone,message=message, user_id=user_id)
+        contact = Contact(listing=listing, listing_id=listing_id, name=name,
+                          email=email, phone=phone, message=message, user_id=user_id)
         contact.save()
-        #Sending mail to realtor
-        send_mail(
-            'Property listing inquiry alert',
-            'Greetings.\n There has been an inquiry for '+listing+' . Sign into the admin panel for more info.',
-            'arnavawasthy61@gmail.com',
-            [ realtor_email , 'arnavawasthy60@gmail.com'],
-            fail_silently=False
-        )   
+        # Sending mail to realtor
+        # send_mail(
+        #     'Property listing inquiry alert',
+        #     'Greetings.\n There has been an inquiry for '+listing+' . Sign into the admin panel for more info.',
+        #     'arnavawasthy61@gmail.com',
+        #     [ realtor_email , 'arnavawasthy60@gmail.com'],
+        #     fail_silently=False
+        # )
 
-        messages.success(request , 'Your request has been successfully submitted! A realtor will get back to you shortly')
+        messages.success(
+            request, 'Your request has been successfully submitted! A realtor will get back to you shortly')
 
         return redirect('/listings/'+listing_id)
--- ttre/settings.py
+++ ttre/settings.py
@@ -149,12 +149,12 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 # Send Email
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'arnavawasthy61@gmail.com'
-EMAIL_HOST_PASSWORD = os.getenv("MAIL_SECRET_KEY")
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_USE_TLS = True
+# EMAIL_PORT = 587
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'arnavawasthy61@gmail.com'
+# EMAIL_HOST_PASSWORD = os.getenv("MAIL_SECRET_KEY")
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 # local settings
 try:
